a428cc2515b6f98ba5759cfe1ff3748f94fee8e4
Akhilakankanala <78506407+Akhilakankanala@users.noreply.github.com>
2021-02-05 22:34:59 +0530

    commit3
    commit3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          welcome to ReactAppdemo
+          welcome to ReactAppdemoone
         </p>
         <a
           className="App-link"
