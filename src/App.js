7e7e1e9bf79010586aac2bce95bd64a6440d584c
Akhilakankanala <78506407+Akhilakankanala@users.noreply.github.com>
2021-02-05 21:25:34 +0530

    changes added
    changes added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit Welome to React App.
+          Edit Welome to React .
         </p>
         <a
           className="App-link"
