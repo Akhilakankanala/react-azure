8f1338af77df63484838d9ce57308fdd0ab59e70
Akhilakankanala <78506407+Akhilakankanala@users.noreply.github.com>
2021-02-06 00:45:50 +0530

    publish
    publish

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          welcome to React App
+          welcome to React
         </p>
         <a
           className="App-link"
